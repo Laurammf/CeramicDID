7dd0e178fd1c4a19f441d31e9a5f9ffe4771e974
Marques Da Fonseca Laura Maria (Student Com19) <lauramaria.marquesdafonseca@stud-inf.unibz.it>
2023-05-30 20:28:03 +0200

    this

--- src/App.js
+++ src/App.js
@@ -2,30 +2,38 @@ import './App.css';
 import { useState } from 'react'
 
 import { DIDSession } from 'did-session'
-import { EthereumNodeAuth, getAccountId } from '@didtools/pkh-ethereum'
-import { ComposeClient }from '@composedb/client'
-import { readEncodedComposite } from '@composedb/devtools-node'
+import { EthereumWebAuth, getAccountId } from '@didtools/pkh-ethereum'
+// import  DID  from 'dids' //removed brackes bc of stackoverflow post#
 import { DID } from 'dids'
-import { Ed25519Provider } from 'key-did-provider-ed25519'
-import { getResolver } from 'key-did-resolver'
-import { fromString } from 'uint8arrays/from-string'
 import { IDX } from '@ceramicstudio/idx'
 import { img } from './images/img'
-import { writeEncodedComposite } from '@composedb/devtools-node'
-import { Composite } from '@composedb/devtools'
 import CeramicClient from '@ceramicnetwork/http-client'
-import ThreeIdResolver from '@ceramicnetwork/3id-did-resolver'
-import { EthereumAuthProvider, ThreeIdConnect } from '@3id/connect'
-import * as fs from 'fs'
+import { ThreeIdConnect } from '@3id/connect'
+import KeyDidResolver from 'key-did-resolver'
+import { Ed25519Provider } from 'key-did-provider-ed25519'
+import * as KeyResolver from 'key-did-resolver'
+import { fromString } from "uint8arrays";
 
-// var fs = require('fs');
 
-//  const endpoint = "http://localhost:7007"                   // 1 
- const endpoint = "https://gateway-clay.ceramic.network"      // 2 
+ const endpoint = "https://localhost:7007"                   // 1 OKO 
+  // const endpoint = "https://gateway-clay.ceramic.network"      // 2 
 // const endpoint = "http://192.168.188.58:7007/"
 //  const endpoint = "https://ceramic-clay.3boxlabs.com"
 
+/**
+ *
+ * Now there is a new error without brackets at import when read profile
+ * 
+ * About the previous error: It was no provider avaliable or sth like this.
+ * on npmjs package info for did it uses this in example code:
+ * 
+ * https://www.npmjs.com/package/dids
+ * 
+ * import { DID } from 'dids'
+import { Ed25519Provider } from 'key-did-provider-ed25519'  <---- this might be needed in combination with the did package
+import * as KeyResolver from 'key-did-resolver'
 
+ */
 
 
 function App() {
@@ -49,18 +57,23 @@ function App() {
 
 
   async function readProfile() {
-    const [address] = await connect()
-    const ceramic = new CeramicClient(endpoint)
+    const ceramic = new CeramicClient(endpoint) 
+    const ethProvider = window.ethereum
+    const addresses = await ethProvider.request({ method: 'eth_requestAccounts' })
+    const accountId = await getAccountId(ethProvider, addresses[0])
+    const authMethod = await EthereumWebAuth.getAuthMethod(ethProvider, accountId)
+    const session = await DIDSession.authorize(authMethod, { resources: [`ceramic://*`]})
 
 
     const idx = new IDX({ ceramic })
         try {
           const data = await idx.get(
            'basicProfile',
-           `eip155:1:${address}`    
+           `eip155:1:${addresses[0]}`    
                               
   )
-
+    console.log(' ')
+    console.log('if catch')
       if (data.name) setName(data.name)
       if (data.avatar) setImage(data.avatar)
     } catch (error) {
@@ -72,90 +85,74 @@ function App() {
   }
 async function updateProfile(){
 
-  // VERSION 1 with updated APIS
-  console.log(' ')
-  console.log('update profile function called')
-  const ceramic = new CeramicClient(endpoint) //create ceramic instance
-  // const client = new ComposeClient(ceramic)
-  
-  const [address] = await connect() // await connection  
-  const accountId = await getAccountId( window.ethereum, address)
+// Version 4 mix match 
 
-  console.log(' ')
-  console.log('const authMethod = await EthereumWebAuth.getAuthMethod(ethProvider, accountId)')
-  const authMethod = await EthereumNodeAuth.getAuthMethod(window.ethereum, accountId) 
-
-  console.log('const composite:')
-
-  const composite = await Composite.fromModels({
-    ceramic: ceramic,
-    // index: false,
-    // models: ['kjz16hvfrbw6ca7nidsnrv78x7r4xt0xki71nvwe4j5a3s9wgou8yu3aj8cz38e'],   // a     INVALID REF
-    models: ['kjzl6hvfrbw6c7keo17n66rxyo21nqqaa9lh491jz16od43nokz7ksfcvzi6bwc']       // b
-    // models: ['k2t6wyfsu4pg2qvoorchoj23e8hf3eiis4w7bucllxkmlk91sjgluuag5syphl'],       // c   refer to a tile
-  })
-  console.log('write encoded composite ')
-
-  // await writeEncodedComposite(composite, 'my-composite.json') PROBLEM HERE
-  console.log("test type"+typeof(composite))
-  
-  const resources = composite.resources
-
-  const session = await DIDSession.authorize(authMethod, {resources})
-
-  const did = new DID({
-    resolver: getResolver(),
-    provider: new Ed25519Provider(), // here inside parenthesis private key
-  })
-  await did.authenticate()
-
-  console.log(' ')
-  console.log('ceramic.setDID')
-  ceramic.setDID(session.did)
-  console.log("end of the code")
+const ceramic = new CeramicClient(endpoint) 
+const ethProvider = window.ethereum
+const addresses = await ethProvider.request({ method: 'eth_requestAccounts' })
+const accountId = await getAccountId(ethProvider, addresses[0])
+const authMethod = await EthereumWebAuth.getAuthMethod(ethProvider, accountId)
 
 
-  //++++++++++++ VERSION 2 WITH OBSOLETE APIS +++++++++++++
-  // const ceramic = new CeramicClient(endpoint)
-  // const client = new ComposeClient({ceramic})
-  // const resources = client.resources
-  // const [address] = await connect()
-  // console.log('await DIDSession.authorize(authMethod, ')
-  // const authMethod = new EthereumAuthProvider(window.ethereum, address)
-  // console.log('session = await DIDSession.authorize(authMethod, {resources})')
-  // const session = await DIDSession.authorize(authMethod, {resources})
-  // console.log('ceramic.did = session.did')
-  // ceramic.did = session.did   
-  // //const session = await DIDSession.authorize(authMethod, { resources: [...]})
-  // console.log('threeIdConnect = new ThreeIdConnect()')
-  // const threeIdConnect = new ThreeIdConnect()
-  // console.log('threeIdConnect.connect(authMethod)')
-  // await threeIdConnect.connect(authMethod)
-  // console.log('const did = new DID({  ')
-  // const did = new DID({       // problem here
-  //   // provider: session.getDidProvider(), 
-  //   resolver: {
-  //     ...ThreeIdResolver.getResolver(ceramic),
-  //     ...getKeyResolver(),
-  //   },
-  // })
-  // //create the DID 
-  // console.log('ceramic.setDID(did) ')
-  // ceramic.setDID(did)
-  // //authenticate user:   
-  // await ceramic.did.authenticate()              // PROBLEM HERE
-  // //create data schema and pass it to ceramic 
-  // const idx = new IDX({ceramic})
-  // console.log('idx ' , idx)
+console.log('session = await DIDSession.authorize(authMethod, {resources})')  
+const session = await DIDSession.authorize(authMethod, { resources: [`ceramic://*`]})
+console.log(' ')
+console.log('ceramic.setDID')
+ceramic.setDID(session.did)                                       // ceramic.did = session.did 
 
-  // //pass the newly received data into the data schema called ix
 
-  // console.log('idx.set')
-  // await idx.set('basicProfile', {
-  //   name, 
-  //   avatar:img
-    
-  // })
+// const resolver = {
+//   ...KeyDidResolver.getResolver(ceramic),
+//   ...ThreeIdResolver.getResolver(ceramic),
+// }
+// const did = new DID({resolver}
+
+// console.log(' ')
+// console.log('instanciate 3id connect')
+// const threeIdConnect = new ThreeIdConnect()
+
+console.log('')
+console.log('seed 4 ')
+const seed4 = fromString(
+    "3686797791467013660892332599998067817022923247599682242140663892",
+    "base16"
+);
+console.log('')
+console.log('provider = new Ed25519Provider(seed4)')
+const provider = new Ed25519Provider(seed4)
+console.log('')
+console.log('const did = new DID({ provider, resolver: KeyResolver.default.getResolver() })')
+const did = new DID({ 
+  provider, 
+  resolver: KeyResolver.default.getResolver() 
+})
+
+
+// const did = new DID({       // problem here
+//   // provider: ethProvider,
+//   provider: authMethod,
+//   resolver: {
+//     ...KeyDidResolver.getResolver(ceramic),
+//     // ...getKeyResolver(),
+//   },
+// })
+
+
+await did.authenticate()   
+ 
+
+console.log(' ')
+console.log('const idx')
+const idx = new IDX({ceramic})
+console.log('idx ' , idx)
+
+console.log('idx.set')
+await idx.set('basicProfile', {
+  name, 
+  avatar:img 
+})
+
+console.log("end of the code")
 
 }
 
@@ -177,3 +174,113 @@ async function updateProfile(){
 }
 
 export default App;
+
+
+
+
+  //   ceramic: ceramic,
+  //   // index: false,
+  //   // models: ['kjz16hvfrbw6ca7nidsnrv78x7r4xt0xki71nvwe4j5a3s9wgou8yu3aj8cz38e'],   // a     INVALID REF
+  //   models: ['kjzl6hvfrbw6c7keo17n66rxyo21nqqaa9lh491jz16od43nokz7ksfcvzi6bwc']       // b
+  //   // models: ['k2t6wyfsu4pg2qvoorchoj23e8hf3eiis4w7bucllxkmlk91sjgluuag5syphl'],       // c   refer to a tile
+  // })
+
+
+  // debugging pattern:
+// console.log(' ')
+// console.log('ceramic.setDID')
+// console.log(session.did) 
+
+
+
+
+
+
+
+// const ceramic = new CeramicClient(endpoint) 
+
+
+// const ethProvider = window.ethereum
+// const addresses = await ethProvider.request({ method: 'eth_requestAccounts' })
+// const accountId = await getAccountId(ethProvider, addresses[0])
+// const authMethod = await EthereumWebAuth.getAuthMethod(ethProvider, accountId)
+
+
+// console.log('session = await DIDSession.authorize(authMethod, {resources})')  
+// const session = await DIDSession.authorize(authMethod, { resources: [`ceramic://*`]})
+// console.log(' ')
+// console.log('ceramic.setDID')
+// ceramic.setDID(session.did)                                       // ceramic.did = session.did 
+// console.log('await authenticate')
+
+// // const resolver = {
+// //   ...KeyDidResolver.getResolver(ceramic),
+// //   ...ThreeIdResolver.getResolver(ceramic),
+// // }
+// // const did = new DID({resolver})
+
+// // const threeIdConnect = new ThreeIdConnect()
+// // await threeIdConnect.setAuthProvider(ethProvider)
+// // await threeIdConnect.connect(accountId)
+
+// const did = new DID({       // problem here
+//   // provider: threeIdConnect.getDidProvider(),
+//   resolver: {
+//     ...KeyDidResolver.getResolver(ceramic),
+//     // ...getKeyResolver(),
+//   },
+// })
+
+// // await did.authenticate({
+// //   provider: ethProvider
+// // })   
+
+// // console.log(' ')
+// // console.log('did debugging:')
+// // console.log(did) 
+
+
+// // console.log(' ')
+// // console.log('did typeof:')
+// // console.log(typeof(did)) 
+
+
+// // console.log(' ')
+// // console.log('did  authenticated?:')
+// // console.log(did.authenticated) 
+
+// // console.log(' ')
+// // console.log('did set provider ethProvider:')
+// // did.setProvider(ethProvider)
+
+
+// // console.log('did provider ')
+// // console.log(did.provider)
+// // console.log(' ')
+
+
+// // await did.authenticate({
+// //   provider: ethProvider
+// // })         // PROBLEM
+
+
+
+// // console.log(' ')
+// // console.log('did  authenticated after did.authenticate() ?:')
+// // console.log(did.authenticated) 
+
+
+// console.log(' ')
+// console.log('const idx')
+// const idx = new IDX({ceramic})
+// console.log('idx ' , idx)
+
+// console.log('idx.set')
+// await idx.set('basicProfile', {
+//   name, 
+//   avatar:img 
+// })
+
+// console.log("end of the code")
+
+// }
